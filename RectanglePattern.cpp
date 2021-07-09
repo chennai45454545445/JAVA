d8d7cb267dee890d146d210ea04400321095c82e
Mohammed sahil ansari <87164152+MdSahilAnsari@users.noreply.github.com>
2021-07-09 06:23:19 -0700

    Add files via upload

---
+++ RectanglePattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    
+   for(int i=0;i<5;i++){
+       for(int j=0;j<4;j++){
+           printf("*");
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
