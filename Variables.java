a3a223795c5422349ae42d005a2e53c6ecbe3ceb
Mohammed sahil ansari <87164152+MdSahilAnsari@users.noreply.github.com>
2021-07-08 22:59:16 -0700

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Variables{
+
+    public static void main(String[] args) {
+	// write your code here
+        // Here 12 is a lit
+        int a=12;
+        System.out.println(a);
+
+    }
+}
