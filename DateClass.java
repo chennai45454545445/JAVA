39a017287d041aa137e1f6b3cb0ef437e5df11d0
Mohammed sahil ansari <87164152+MdSahilAnsari@users.noreply.github.com>
2021-11-18 21:55:36 +0530

    Add files via upload

---
+++ DateClass.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.*;
+public class DateClass {
+    public static void main(String[] args) {
+     Date d=new Date();
+        System.out.println(d);
+        System.out.println(d.getYear());
+        System.out.println();
+    }
+}
